a9fa07f510084382e83ffec308c1f148cec895c5
SolidW05 <corona040502@gmail.com>
2024-10-17 22:19:49 -0600

    prototipo

---
+++ src/main/java/DemoInv/VentanaPrincipal.java
@@ -0,0 +1,42 @@
+package DemoInv;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VentanaPrincipal extends JFrame{
+    private JPanel panel1;
+    private JPanel panelBotones;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JPanel paneles;
+    private JButton tablaStock;
+    private JPanel panelProductos;
+    private JPanel panelTrabajadores;
+    private JPanel panelStock;
+
+    public VentanaPrincipal(){
+        add(panel1);
+        pack();
+
+        paneles.add(panelTrabajadores, "panelTrabajadores");
+        paneles.add(panelProductos, "panelProductos");
+        paneles.add(panelStock, "panelStock");
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cl = (CardLayout) (paneles.getLayout());
+                cl.show(paneles, "panelStock");
+            }
+        });
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new VentanaPrincipal();
+    }
+}
